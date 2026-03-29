cc3aeb592a7a00c3045b8cf970f02c7955a8716d
bhutuklearning <amritanshugoutam.dev@gmail.com>
2026-03-29 15:15:22 +0530

    Backend compatible error fixed.

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,7 +49,6 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
